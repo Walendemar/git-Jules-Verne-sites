b81716e832ea88e00d7957bd8f9045a23d2c52a1
Vladimir Gordiencko <forstudiesandetc@gmail.com>
2020-12-28 14:10:27 +0300

    clicking on the icon adds highlighting

--- scripts/slider-newbook.js
+++ scripts/slider-newbook.js
@@ -7,15 +7,36 @@ let panePosition = 0;
 // Количество элементов
 let elementsCount = newbookUl.querySelectorAll('li').length;
 
+// Ставлю обработчик на клик по панеле
+newbookUl.addEventListener('click', ulClick);
+
 // Ставлю обработчик на клик по стрелкам
 buttonsPane.addEventListener('click', buttonClick);
 
-// Функция для обработчика
+// Функция для обработчика newbookUl
+function ulClick(event) {
+
+    // Текущий div с картинкой
+    let div = event.target.closest('.pane');
+
+    // Если клик не по панеле с картинкой
+    if  (!div) {
+        return;
+    }
+
+    // Меняем подсветку
+    div.classList.toggle('selected');
+
+    // Меняем текст кнопки
+    switchText(div.classList.contains('selected') ? true : false);
+}
+
+// Функция для обработчика buttonsPane
 function buttonClick(event) {
 
     // Текущий div с картинкой
     let div = newbookUl.children[panePosition].children[0];
-    
+
     // Если клик по левой кнопке
     if (event.target.classList.contains('toleft') && panePosition > 0) {
         panePosition--;
@@ -34,17 +55,17 @@ function buttonClick(event) {
     // Если по кнопке добавить
     if (event.target.classList.contains('button')) {
         // Добавляю div'у с картинкой класс, меняющий подсветку и рамки
-        
         div.classList.toggle('selected');
         // Меняю текст кнопки
         switchText(div.classList.contains('selected') ? true : false);
     }
 
-    function switchText(status) {
-        buttonsPane.querySelector('.button').innerHTML = status ? "УБРАТЬ" : "ДОБАВИТЬ";
-    }
-
     // Двигаем слайдер
     newbookUl.style.marginLeft = -panePosition * 840 + 'px';
+}
+
+// Функция замены текста на кнопке
+function switchText(status) {
+    buttonsPane.querySelector('.button').innerHTML = status ? "УБРАТЬ" : "ДОБАВИТЬ";
 }
 
