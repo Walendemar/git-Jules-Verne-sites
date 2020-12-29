12ad19b344ad8be9b270d97d2e12c9957736de83
Vladimir Gordiencko <forstudiesandetc@gmail.com>
2020-12-29 17:00:35 +0300

    adding new function

--- scripts/handlers.js
+++ scripts/handlers.js
@@ -1,4 +1,11 @@
-// Функция замена текста на кнопке
+// Функция замены текста на кнопке
 function switchText(status, button) {
     button.innerHTML = status ? "УБРАТЬ" : "ДОБАВИТЬ";
+}
+
+// Функция замены цвета указателя
+function switchCursorColor(status, cursor) {
+    
+    cursor.src = status ?  'icons/cursor-orange.png' : 'icons/cursor.png';
+    cursor.classList.toggle('changed');
 }
