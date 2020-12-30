ff3a57f4faa13e274c513b3161747f2a2ebb60e6
Vladimir Gordiencko <forstudiesandetc@gmail.com>
2020-12-30 19:35:31 +0300

    refactoring, replacement of an internal function with a global

--- scripts/to-top-button.js
+++ scripts/to-top-button.js
@@ -2,17 +2,6 @@ let upButton = document.getElementById('upButton');
 
 upButton.onclick = function(event) {
 
-    // Функция для прокрутки
-    function scrollHandler(timerId) {
-        let coordY = document.body.getBoundingClientRect().top;
-
-        window.scrollBy(0, -50);
-
-        if (coordY >= 0) {
-            clearInterval(timerId);
-        }
-    }
-
     // Делаем анимацию для прокрутки
-    let timerId = setInterval(() => scrollHandler(timerId), 5);
+    let timerId = setInterval(() => scrollTo(timerId, -400, 50, -1), 10);
 }
