4af4807ceeda8f6df1302d6bceeacf589c7a94db
Vladimir Gordiencko <forstudiesandetc@gmail.com>
2020-12-30 19:32:05 +0300

    add new function

--- scripts/handlers.js
+++ scripts/handlers.js
@@ -9,3 +9,21 @@ function switchCursorColor(status, cursor) {
     cursor.src = status ?  'icons/cursor-orange.png' : 'icons/cursor.png';
     cursor.classList.toggle('changed');
 }
+
+// Функция для прокрутки
+// position - координаты элемента
+// speed - скорость перемещения
+// direction - направление движения экрана
+function scrollTo(timerId, position, speed, direction) {
+    let windowPosition = document.body.getBoundingClientRect().top;
+
+    window.scrollBy(0, speed * direction);
+
+    if (windowPosition >= position && direction < 0) {
+        clearInterval(timerId);
+    }
+
+    if (windowPosition <= -position && direction > 0) {
+        clearInterval(timerId);
+    }
+}
