2d3257a8680e198855e767b73e82f9d0098090a0
Vladimir Gordiencko <forstudiesandetc@gmail.com>
2021-01-06 16:20:32 +0300

    fix bug with scrolling

--- scripts/nav-menu-scroller.js
+++ scripts/nav-menu-scroller.js
@@ -12,6 +12,7 @@ function scrollMenu(event) {
 
     // Позиция секции
     let position;
+    let scrollY = window.scrollY;
     // Для каждого элемента вызываем функцию и передаем туда параметры
     switch(event.target) {
         case navPanel.children[0]:
@@ -19,28 +20,28 @@ function scrollMenu(event) {
             position = document.getElementById('aboutUs')
                                 .getBoundingClientRect()
                                 .top;
-            timerId = setInterval(() => scrollTo(timerId, position-15, 10, 1), 5);
+            timerId = setInterval(() => scrollTo(timerId, position + scrollY, 10, 1), 5);
             break;
         case navPanel.children[1]:
             // "Каталог"
             position = document.getElementById('price')
                                .getBoundingClientRect()
-                               .top;
-            timerId = setInterval(() => scrollTo(timerId, position-27, 20, 1), 10);
+                               .top - 26;
+            timerId = setInterval(() => scrollTo(timerId, position + scrollY, 20, 1), 10);
             break;
         case navPanel.children[2]:
             // "Новинки"
             position = document.getElementById('newbook')
                                .getBoundingClientRect()
-                               .top;
-            timerId = setInterval(() => scrollTo(timerId, position-30, 30, 1), 10);
+                               .top - 30;
+            timerId = setInterval(() => scrollTo(timerId, position + scrollY, 30, 1), 10);
             break;
         case navPanel.children[3]:
             // "Контакты"
             position = document.getElementById('contacts')
                                .getBoundingClientRect()
-                               .top;
-            timerId = setInterval(() => scrollTo(timerId, position-70, 40, 1), 10);
+                               .top - 55;
+            timerId = setInterval(() => scrollTo(timerId, position + scrollY, 40, 1), 10);
             break;
     }
 }
